2898671e6f91e57f6f701038ebe4d6e2e84a795a
Carlos Barreiro <carlosbarreiro2604@gmail.com>
2021-03-22 20:04:31 +0000

    Commit Aula 4

---
+++ Aulas-Angular-main/Aulas-Angular-main/aulas/src/app/experiments/experiments.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiments',
+  templateUrl: './experiments.component.html',
+  styleUrls: ['./experiments.component.css']
+})
+export class ExperimentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  name : string ="Moisés";
+
+  changeName(event){
+    this.name = event.target.value;
+  }
+
+
+}
